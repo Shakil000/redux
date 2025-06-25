b4192af0ca63edecac550bcc220835af0f8cc00e
Shakil000 <racer.shakil111@gmail.com>
2025-06-25 15:43:22 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css'
+import { decrement, increment } from './features/counter/counterSlice';
+
+function App() {
+  const dispatch = useDispatch();
+  const {count} = useSelector((state) => state.counter);
+
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  }
+
+  return (
+    <div>
+      <h1>Creating counter with redux</h1>
+
+      <button onClick={handleIncrement}>Increment</button>
+      <div>{count}</div>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App
+
+
+// 3.32s
